12b674d51c214381143bb8515a477ea78d791659
Johnathan Villa Castruita <88985846+VillaCodes@users.noreply.github.com>
2021-12-22 02:45:56 -0800

    Initial commit with completed algorithms

--- src/intersection/index.js
+++ src/intersection/index.js
@@ -1,3 +1,24 @@
-function intersection(a, b) {}
+function intersection(a, b) {
+
+    const aMap = new Map();
+
+    const results = new Set();
+
+    //iterate over each element below and add it to the map
+
+    for (let e of a) {
+        aMap.set(e, e)
+    }
+
+    //check if map includes an element of b, and add it to set if so
+
+    for (let e of b) {
+        if (aMap.has(e)){
+            results.add(e)
+        }
+    }
+
+    return [...results] //Array.from(results)
+}
 
 module.exports = intersection;
--- src/sumPairs/index.js
+++ src/sumPairs/index.js
@@ -3,6 +3,27 @@
  * @param {Array} A an array of numbers
  * @param {Number} N an integer value
  */
-function sumPairs(A, N) {}
+function sumPairs(A, N) {
+
+    let numbers = new Map();
+    let solution = new Map();
+
+    for (let e of A) {
+        numbers.set(e, e)
+    }
+
+    for (let e of A) {
+        let difference = N - e;
+
+        //make sure N doesn't create a pair of duplicate numbers by being even
+        if (difference !== e) {
+            if (numbers.has(difference)) {
+                solution.set(Math.min(e, difference), Math.max(e,difference))
+            }
+        }
+    }
+
+    return Array.from(solution)
+}
 
 module.exports = sumPairs;
